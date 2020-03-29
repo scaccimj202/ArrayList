fc39feca9d8d327c0f72989db54e97e96dc461c1
Matt Scaccia <scaccimj202@potsdam.edu>
2020-03-29 12:09:55 -0400

    Added ListADT to directory

---
+++ AList.java
@@ -0,0 +1,8 @@
+/**
+ *@author Matthew Scaccia
+ *course CIS303
+ *@since 3/29/2020
+ *assignment 4b
+ *description: ArrayList data structure, modeled after Data Structures
+ *and Algorithim Analysis by Clifford Shaffer Chapter 4.1.1
+ */
