6471a4132b2ea93585ae8275c1726aa3e89a4345
Matt Scaccia <scaccimj202@potsdam.edu>
2020-03-29 12:17:50 -0400

    Added constructors and clear methods to ArrayList

--- AList.java
+++ AList.java
@@ -6,3 +6,39 @@
  *description: ArrayList data structure, modeled after Data Structures
  *and Algorithim Analysis by Clifford Shaffer Chapter 4.1.1
  */
+
+ public class ArrayList<E> implements List<E>{
+
+    private static final int defaultSize = 10;
+    private int maxSize;
+    private int listSize;
+    private int currentPosition;
+    private E[] listArray;
+
+    /**
+     * Constructor with list default capacity
+     */
+    ArrayList(){
+        this(defaultSize);
+    }
+    
+    /**
+     * Constructor with new list object
+     * @param size the maxSize of the ArrayList
+     */
+    @SuppressWarnings("unchecked")
+    ArrayList(int size){
+        maxSize = size;
+        listSize = currentPosition = 0;
+        listArray = (E[]) new Object[size];
+    }
+
+    /**
+     * Method clears the list
+     */
+    public void clear(){
+        listSize = currentPosition = 0;
+    }
+
+    
+ }
